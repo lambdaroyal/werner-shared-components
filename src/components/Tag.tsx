f53ac083349af96019da0ac161dd6af3b7a9bb0d
gixxi <christian.meichsner@live.com>
2024-07-21 23:32:54 +0200

    working for werner-server with tsup

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -10,7 +10,7 @@ import { isNotEmpty } from '../lib/utils'
 import { CopyHandle } from './CopyHandle'
 
 export const TagVariantStyles = {
-  small: 'rounded-lg ring-1 ring-inset py-0.5',
+  small: 'rounded-lg ring-1 ring-inset py-0.25',
   medium: 'rounded-lg ring-1 ring-inset py-0.5 text-sm',
 }
 
